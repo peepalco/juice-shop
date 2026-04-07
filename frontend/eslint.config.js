5cf1e9b3977386f120812b80ee16503dad18c35d
anuraagpathakk1 <anurag.p@peepal.co>
2026-04-07 20:40:43 +0530

    Update eslint.config.js
    Signed-off-by: anuraagpathakk1 <anurag.p@peepal.co>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop jhjhjhj.
  * SPDX-License-Identifier: MIT
  */
 
